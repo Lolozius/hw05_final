35902d7a203dd66d1b880125a342a563486018f4
Сергей Юдаев <bat.ru2013@mail.ru>
2022-07-20 13:45:31 +0300

    чек поинт

--- yatube/users/views.py
+++ yatube/users/views.py
@@ -6,7 +6,7 @@ from .forms import CreationForm
 
 class SignUp(CreateView):
     form_class = CreationForm
-    success_url = reverse_lazy('posts:login')
+    success_url = reverse_lazy('users:login')
     template_name = 'users/signup.html'
 
 
